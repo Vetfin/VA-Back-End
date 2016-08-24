16b1d3849fcf1a9eca659015d08e19cb6a88371a
Erik Douglas <eadouglas10@gmail.com>
2016-08-24 15:33:38 -0400

    Allowed scraper to access unsecured sites

--- app/controllers/condos_controller.rb
+++ app/controllers/condos_controller.rb
@@ -1,7 +1,8 @@
 class CondosController < ApplicationController
   def index
-    Condo.destroy_all
+    # Condo.destroy_all
     VaScrape.new
     @condos = Condo.all
   end
+
 end
--- app/jobs/va_scrape.rb
+++ app/jobs/va_scrape.rb
@@ -7,6 +7,7 @@ class VaScrape
 
   def initialize
     agent = Mechanize.new
+    agent.verify_mode = OpenSSL::SSL::VERIFY_NONE
     @page = agent.get("https://vip.vba.va.gov/portal/VBAH/VBAHome/condopudsearch?paf_portalId=default&paf_communityId=100002&paf_pageId=500002&paf_dm=full&paf_gear_id=800001&paf_gm=content&paf_ps=_rp_800001_condoName%3D1_%26_rp_800001_approvedOnly%3D1_yes%26_rp_800001_condoId%3D1_%26_ps_800001%3Dmaximized%26_pid%3D800001%26_rp_800001_county%3D1_%26_rp_800001_stateCode%3D1_DC%26_md_800001%3Dview%26_rp_800001_cpbaction%3D1_performSearchPud%26_st_800001%3Dmaximized%26_rp_800001_reportType%3D1_details%26_rp_800001_regionalOffice%3D1_%26_rp_800001_city%3D1_&_requestid=564820")
     persist(get_addresses)
   end
