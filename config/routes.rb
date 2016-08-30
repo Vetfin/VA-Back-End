33fc5becd344ca76e22fa142af7beb7f4bb8d747
Erik Douglas <eadouglas10@gmail.com>
2016-08-30 16:31:12 -0400

    Made routes match resourceful conventions

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  get 'condos/index'
-
-  get 'addresses/index'
 
   root 'welcome#index'
 
+  resources :buildings, only: [:index, :show]
+  resources :addresses, only: [:index]
   resources :condos, only: [:index]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
