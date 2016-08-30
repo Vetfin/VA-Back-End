97f692eb4289d8f1516366deff7dc9105b8eb490
Erik Douglas <eadouglas10@gmail.com>
2016-08-30 16:00:17 -0400

    Validated address model to only be generated by successfully geocoded buildings

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,6 @@ class Address < ApplicationRecord
   reverse_geocoded_by :latitude, :longitude
   before_validation :reverse_geocode
   validates :address, uniqueness: true
+  validates :latitude, presence: true
   has_many :condos
 end
