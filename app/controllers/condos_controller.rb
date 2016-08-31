f13962ad28db8e62155aeeb4b396ab08607145e7
Erik Douglas <eadouglas10@gmail.com>
2016-08-31 15:01:40 -0400

    Added search to condos

--- app/controllers/condos_controller.rb
+++ app/controllers/condos_controller.rb
@@ -3,4 +3,13 @@ class CondosController < ApplicationController
     @condos = Condo.all
   end
 
+  def search
+    @condos = Condo.where("price <? AND price >?", search_params[:max_price], search_params[:min_price])
+    render :index
+  end
+
+  private def search_params
+    params.require(:search).permit(:zipcode, :city, :min_price, :max_price, :min_feet, :max_feet, :min_bed, :max_bed, :min_bath, :max_bath, :parking, :min_hoa, :max_hoa)
+  end
+
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -1,13 +1,5 @@
 class SearchesController < ApplicationController
 
-  def create
-    @search = Search.new(search_params)
-    @condos = Condo.where("price <? AND price >?", @search.max_price, @search.min_price)
-    render condos_path
-  end
 
-  private def search_params
-    params.require(:search).permit(:zipcode, :city, :min_price, :max_price, :min_feet, :max_feet, :min_bed, :max_bed, :min_bath, :max_bath, :parking, :min_hoa, :max_hoa)
-  end
 
 end
