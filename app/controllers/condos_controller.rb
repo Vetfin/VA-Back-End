eae93588c5ed168e5a3b73bcfc1bdf0608400edf
Erik Douglas <eadouglas10@gmail.com>
2016-08-31 15:14:49 -0400

    Changed param requirements

--- app/controllers/condos_controller.rb
+++ app/controllers/condos_controller.rb
@@ -9,7 +9,7 @@ class CondosController < ApplicationController
   end
 
   private def search_params
-    params.require(:search).permit(:zipcode, :city, :min_price, :max_price, :min_feet, :max_feet, :min_bed, :max_bed, :min_bath, :max_bath, :parking, :min_hoa, :max_hoa)
+    params.require(:condo).permit(:zipcode, :city, :min_price, :max_price, :min_feet, :max_feet, :min_bed, :max_bed, :min_bath, :max_bath, :parking, :min_hoa, :max_hoa)
   end
 
 end
