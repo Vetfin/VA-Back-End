43ebb004bf3e31a06bfcd56c56b2cdeec0285bf4
Erik Douglas <eadouglas10@gmail.com>
2016-09-04 15:45:42 -0400

    Added search param options

--- app/controllers/condos_controller.rb
+++ app/controllers/condos_controller.rb
@@ -4,7 +4,7 @@ class CondosController < ApplicationController
   end
 
   def search
-    @condos = Condo.where("price <? AND price >?", params[:max_price], params[:min_price])
+    @condos = Condo.where("status = ? AND price <? AND price >? AND beds < ? AND beds > ? AND baths < ? AND baths > ?", "For Sale", params[:max_price], params[:min_price], params[:max_bed], params[:min_bed], params[:max_bath], params[:min_bath])
     render :index
   end
 
