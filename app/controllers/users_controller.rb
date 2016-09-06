3f4f4f013fa1082eec2be9e4db918ac99277a203
Erik Douglas <eadouglas10@gmail.com>
2016-09-06 14:38:08 -0400

    Returning json when posting new user or login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,16 +2,18 @@ class UsersController < ApplicationController
 
   def create
     @user = User.create(user_params)
-    redirect_to user_path(@user.id)
+    render json: @user
   end
 
   def show
     @user = User.find(params[:id])
+    @condos = User.condos
   end
 
   def login
     @user = User.find_by(params[:email])
-    redirect_to user_path(@user.id)
+    # redirect_to user_path(@user.id)
+    render json: @user
   end
 
   private def user_params
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -1,7 +1,8 @@
 require 'test_helper'
 
 class UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can create user" do
+    post users_url, user: { first_name: "Erik", last_name: "Douglas", email: "erik@erik"}
+    assert_response :success
+  end
 end
