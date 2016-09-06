b877e95d90eacea994af5ac2b617744a88dc49ed
Erik Douglas <eadouglas10@gmail.com>
2016-09-06 15:37:44 -0400

    Chained search parameters

--- app/controllers/condos_controller.rb
+++ app/controllers/condos_controller.rb
@@ -5,9 +5,13 @@ class CondosController < ApplicationController
 
   def search
     # @condos = Condo.where("price < ? AND price > ? AND beds < ? AND beds > ? AND baths < ? AND baths > ?", params[:max_price], params[:min_price], params[:max_bed], params[:min_bed], params[:max_bath], params[:min_bath])
-    @condos = Condo \
-      .where("price >= ?", params[:min_price]) if params[:min_price]
-
+    @condos = Condo.all
+    @condos = @condos.where("price >= ?", params[:min_price]) if params[:min_price]
+    @condos = @condos.where("price <= ?", params[:max_price]) if params[:max_price]
+    @condos = @condos.where("baths >= ?", params[:min_bath]) if params[:min_bath]
+    @condos = @condos.where("baths <= ?", params[:max_bath]) if params[:max_bath]
+    @condos = @condos.where("beds >= ?", params[:min_bed]) if params[:min_bed]
+    @condos = @condos.where("beds <= ?", params[:max_bed]) if params[:max_bed]
 
     render :index
   end
